ec4e7b1c7e3023bbcd484d37d68974f8c42d70aa
yuran1811 <trieuvanbd123@gmail.com>
2022-07-24 15:47:46 +0700

    update

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### Technologies
 
--   TailwindCSS
+- TailwindCSS
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      sans: ['Fira sans', 'sans-serif'],
+    },
+  },
+};
